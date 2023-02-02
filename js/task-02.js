28335338294b95b699829ec726841b3f3767579d
Mariia Sakhnenko <mm.maryska@gmail.com>
2023-02-02 21:46:56 +0200

    Completed 1 and 2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,14 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const listIngredients = document.querySelector('ul#ingredients');
+
+const listItems = ingredients.map((ingredient) => {
+  const item = document.createElement('li');
+  item.textContent = ingredient;
+  item.classList.add('item');
+  return item;
+});
+
+listIngredients.append(...listItems);
