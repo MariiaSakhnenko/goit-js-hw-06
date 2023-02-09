8baa1012781e2a811b840669ac51f6798137dad9
Mariia Sakhnenko <mm.maryska@gmail.com>
2023-02-09 20:29:10 +0200

    Update

--- js/task-06.js
+++ js/task-06.js
@@ -1,32 +1,14 @@
-// Напиши скрипт, який під час втрати фокусу на інпуті (подія blur), перевіряє його вміст щодо правильної кількості введених символів.
-// <input
-//   type="text"
-//   id="validation-input"
-//   data-length="6"
-//   placeholder="Please enter 6 symbols"
-// />
-// Яка кількість символів повинна бути в інпуті, зазначається в його атрибуті data-length.
-// Якщо введена правильна кількість символів, то border інпуту стає зеленим, якщо неправильна кількість - червоним.
-// Для додавання стилів використовуй CSS-класи valid і invalid, які ми вже додали у вихідні файли завдання.
+const inputRef = document.querySelector('#validation-input');
 
-
-
-document.querySelector('body').style.backgroundColor = '#EBECF0';
-const inputRef = document.getElementById('validation-input');
-inputRef.addEventListener('blur', onBlurBorderColor);
-
-function onBlurBorderColor(event) {
-    const inputValue = inputRef.getAttribute('data-length');
-    
-    if (inputRef.value.length === Number(inputValue)) {
-        inputRef.classList.add('valid');
-        inputRef.classList.remove('invalid');
-    }
-    if (inputRef.value.length === 0) {
-        inputRef.classList.remove('valid');
-        inputRef.classList.remove('invalid');
-    }
-    if (inputRef.value.length !== Number(inputValue) && inputRef.value.length !== 0) {
-        inputRef.classList.add('invalid');
-    }
+const onBlurBorderColor = () => {
+if (inputRef.value.length === Number(inputRef.dataset.length)) {
+        inputRef.classList.remove(`invalid`);
+        inputRef.classList.add(`valid`);
+        return;
+    } else {
+        inputRef.classList.remove(`valid`);
+        inputRef.classList.add(`invalid`);
+    };
 }
+
+inputRef.addEventListener('blur', onBlurBorderColor);
